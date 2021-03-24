3450761b5fc816293de67b1eab6de286a90ca274
박상현 <airwalk741@naver.com>
2021-03-25 01:01:50 +0900

    3/25

---
+++ algorithm/3월/3월 24일/암호문1.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.stdin = open('input (43).txt', 'r')
+
+
+for tc in range(1, 11):
+
+    n = int(input())
+
+    result_num = list(input().split())
+
+    k = int(input())
+
+    num = list(input().split())
+
+    while len(num):
+        num.pop(0)
+        idx = int(num.pop(0))
+        length = num.pop(0)
+
+        for i in range(int(length)):
+            result_num.insert(idx+i, num.pop(0))
+
+
+    print('#{} {}'.format(tc, ' '.join(result_num[:10])))
+
+
+
+
+
+
+
+
+
+
+
