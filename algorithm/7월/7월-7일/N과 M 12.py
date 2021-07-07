6906f8c7fa2df5c0c36b17a01613629cf91b13e1
박상현 <airwalk741@naver.com>
2021-07-07 20:56:53 +0900

    N과 M (12)

---
+++ algorithm/7월/7월-7일/N과 M 12.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('input (43).txt', 'r')
+
+
+def search(idx, res, cnt):
+
+    if cnt == m:
+        if res not in result:
+            print(res)
+            result.add(res)
+
+    else:
+        for i in range(idx, n):
+
+            search(i, res + arr[i] + ' ', cnt + 1)
+
+
+
+n, m = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+arr = list(map(str, arr))
+
+result = set()
+
+search(0, '', 0)
+
+
