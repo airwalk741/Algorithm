78d8e1bb4fefe1f1cb6420ae25c3cdac906c4185
DESKTOP-KVCQHCD\multicampus <airwalk741@naver.com>
2021-08-07 11:06:02 +0900

    백준 보물섬 다시 풀이

--- algorithm/8월/8월-6일/README.md
+++ algorithm/8월/8월-6일/README.md
@@ -33,5 +33,86 @@ WLLWLWL
 - 아래 테케에서는 `9`를 반환해줘야 할 것 같은데 내 풀이는 `8`을 반환해준다.
 - 다시 풀어봐야지...
 
+```py
+from collections import deque
+import pprint
+
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+
+def bfs(r, c):
+    global first_dist
+    global first
 
 
+    q = deque()
+    q.append((r, c, 0))
+
+
+    while q:
+
+        r, c, dist = q.popleft()
+
+        if dist > first_dist:
+            first_dist = dist
+            first.append((r, c))
+
+        for i in range(4):
+
+            x = r + dr[i]
+            y = c + dc[i]
+
+            if -1 < x < n and -1 < y < m:
+                if not visited[x][y]:
+                    if arr[x][y] == 'L':
+                        visited[x][y] = visited[r][c] + 1
+                        q.append((x, y, visited[x][y]))
+
+
+
+n, m = map(int, input().split())
+
+arr = [list(input()) for _ in range(n)]
+
+first_dist = 0
+first = []
+
+visited = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j]:
+            if arr[i][j] == 'L':
+                visited[i][j] = 1
+                bfs(i, j)
+
+first_dist = 0
+
+second = []
+length = len(first)
+
+for i in range(length):
+
+    r, c = first[i][0], first[i][1]
+    first_dist = 0
+    visited = [[0] * m for _ in range(n)]
+    visited[r][c] = 1
+    bfs(r, c)
+    second.append(first_dist)
+
+
+print(max(second) - 1)
+
+```
+
+
+
+#### ✍ 두 번째 풀이
+
+- 구석 좌표들을 다 구해서 리스트에 담아둔다.
+- 구석 좌표들을 하나씩 꺼내서 최대 길이를 구한다.
+- 구석 좌표 예시
+
+![image-20210807110455030](README.assets/image-20210807110455030.png)
+
+![image-20210807110418818](README.assets/image-20210807110418818.png)
--- algorithm/8월/8월-6일/보물섬.py
+++ algorithm/8월/8월-6일/보물섬.py
@@ -1,76 +1,78 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 from collections import deque
-import pprint
-
-## 먼저 하나의 위치에서 BFS를 돌리고 기록이 갱신 될때마다 현 위치 좌표를 담는다.
-## 위치 담은 좌표들을 다시 BFS를 돌려 최대 처리를 구한다.
 
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-def bfs(r, c):
-    global first_dist
-    global first
+## 처음에 구석인 좌표들만 second에 담는다
+## second에 담아진 좌표들 하나씩 꺼내어 최대 길이를 구해서 result 변수에 담는다.
 
+def bfs(r, c, second_check):  ## second_check는 함수 재사용하기 위한 변수 (1일 때 구석 좌표 구하기, 0일 때는 구하지 않기)
+    global result
 
     q = deque()
     q.append((r, c, 0))
-
-
+    second.append((r, c))
+    _max = 0
+    visited[r][c] = 1
     while q:
 
-        r, c, dist = q.popleft()
+        r, c, cnt = q.popleft()
 
-        if dist > first_dist:
-            first_dist = dist
-            first.append((r, c))
+        if _max < cnt:
+            _max = cnt
+
+
+        check = 1
 
         for i in range(4):
-
             x = r + dr[i]
             y = c + dc[i]
 
             if -1 < x < n and -1 < y < m:
                 if not visited[x][y]:
                     if arr[x][y] == 'L':
-                        visited[x][y] = visited[r][c] + 1
-                        q.append((x, y, visited[x][y]))
+                        visited[x][y] = 1
+                        q.append((x, y, cnt + 1))
+                        check = 0 ## 다음 좌표 추가되면 구석이 아니다
 
+        if second_check:
+            if check:
+                second.append((r, c))
+
+    result.append(_max) # 최대 길이 담아두기
 
 
 n, m = map(int, input().split())
 
 arr = [list(input()) for _ in range(n)]
 
-first_dist = 0
-first = []
 
 visited = [[0] * m for _ in range(n)]
 
+result = []
+
+second = deque()
+
+## 구석 좌표구하기
 for i in range(n):
     for j in range(m):
-        if not visited[i][j]:
-            if arr[i][j] == 'L':
-                visited[i][j] = 1
-                bfs(i, j)
+        if arr[i][j] == 'L':
+            if not visited[i][j]:
+                bfs(i, j, 1)
 
-first_dist = 0
 
-second = []
-length = len(first)
-pprint.pprint(visited)
-print(first)
+## 구석 좌표들에서 최대 길이 구하기
+length = len(second)
+for _ in range(length):
 
-for i in range(length):
+    r, c = second.popleft()
 
-    r, c = first[i][0], first[i][1]
-    first_dist = 0
     visited = [[0] * m for _ in range(n)]
-    visited[r][c] = 1
-    bfs(r, c)
-    second.append(first_dist)
+
+    bfs(r, c, 0)
 
 
-print(max(second) - 1)
+print(max(result))
 
